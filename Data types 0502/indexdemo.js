ecc4fb4defb2ee931fa8a07d7402f0220208dfbd
Rahul-N-2389 <rahulneelam2389@gmail.com>
2026-04-08 11:26:52 +0530

    Data types 05/02

---
+++ Data types 0502/indexdemo.js
@@ -0,0 +1,25 @@
+// let age = prompt("enter your age")
+// console.log(10-age);
+
+
+
+/* converting int0 different data types */
+// Number()
+// Boolean()
+// String()
+
+// var a = Boolean("");  // false = 0
+// var b = 1;            // 1
+// console.log(a+b);     0+1 =1
+
+
+// var a = Boolean(" ");   //true = 1
+// var b = 1;              //1
+// console.log(a+b);       //1+1 = 2
+
+
+let age1 = prompt("enter your age ")
+let age2 = prompt("enter your age ")
+console.log(Number(age1)+Number(age2));
+
+
