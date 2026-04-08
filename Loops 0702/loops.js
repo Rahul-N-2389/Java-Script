de29d240b0228d52f88ca72cc1c8450ec74894c9
Rahul-N-2389 <rahulneelam2389@gmail.com>
2026-04-08 12:06:19 +0530

    loops

---
+++ Loops 0702/loops.js
@@ -0,0 +1,15 @@
+
+
+
+debugger;
+let laptop = true
+
+if(laptop){
+    console.log("Attended Zoom classes");
+    
+    if(laptop){
+        console.log("Not attended Zoom classes");
+        
+    }
+    
+}
