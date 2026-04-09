aadc7a5e548744cf73cfd61f7dd65fa348dc45fe
Rahul-N-2389 <rahulneelam2389@gmail.com>
2026-04-09 12:09:53 +0530

    named function 10,11/02/26

---
+++ namedFunction 1002/named.js
@@ -0,0 +1,176 @@
+/**
+ * named function
+ * ---------------------------------
+ * fun definition
+ * ---------------
+ *     function fun_Name([parameters......]){
+ *     //statements
+ *      [return]
+ *     }
+ * 
+ * note:- Function definition always stored in a fun_name
+ * Ex:- log(fun_name);
+ * 
+ * fun calling
+ * ------------
+ * fun_name();
+ */
+
+// function hello(){
+//     console.log("helle there");
+// }
+// console.log(hello);  //fun definition
+// hello();             //fun output
+
+// function sum(){
+//     console.log(10+20);
+    
+// }
+// console.log(sum());
+
+
+// function kitchen(){
+//     console.log("cooking started");
+//     order();
+//     console.log("cooking stoped");
+// }
+// function order(){
+//     console.log("order taken");
+//     console.log("went to kitchen");
+    
+    
+// }
+// kitchen();
+
+
+// var a;
+// function getNum(){
+//     console.log(a+10); //nothin+num = NAN
+    
+// }
+// getNum();
+
+
+// let a;
+// function getNum(){
+//     console.log(a+10); //nothin+num = NAN
+    
+// }
+// getNum();
+
+
+/* using parameters in named functions */
+// ------------------------------------------
+
+// function getmaggie(maggie){
+//     console.log(`2 pachets of ${maggie} ready to open`)
+    
+// }
+// getmaggie("yipeee");
+
+// function biryani(persons,rate){
+//     console.log(persons*rate);
+    
+// }
+// biryani(5,150)
+
+// function pushpa(hero,sidechar){
+//     console.log(hero);
+//     console.log(sidechar);
+//     devara("ntr","prakash")
+//     console.log(hero+sidechar);
+//     devara(hero,sidechar)
+// }
+// function devara(hero,villain){
+//     console.log(hero+villain);
+// }
+// pushpa("allu","pk");
+
+
+
+
+
+// function sum1(a,b,c){
+//     console.log(a+b+sum2(a));
+//     console.log(c);
+// }
+// function sum2(d){
+//     console.log(d+20);
+// }
+// sum1(10,20,30)
+/**
+ * 30
+ * NAN
+ * 30
+ */
+
+
+
+// function print(a,b,c){
+//     console.log(a+b+(c+a));
+//     console.log(c);
+//     printer(c+20);  
+// }
+// function printer(d){
+//     console.log(d+34);
+// }
+// print(34,43,5)
+
+
+/** using return in named function  */
+
+
+// function print(){
+//     return "hello there"
+// }
+// console.log(print());
+
+
+
+
+
+// function print (a,b,c){
+//     console.log(a+b+sub(c));
+//     sub(a)  
+// }
+// function sub(a){
+//     return a+20;
+// }
+// console.log(print(10,20,30));
+
+
+// var a = 10;
+// function print(){
+//     console.log(a);
+    
+//     console.log(x);
+//     return "hello world"
+//     var x = 10;
+// }
+// print();   // undefine
+
+
+
+// function print(){
+//     var a = 10;
+//     console.log(a);
+    
+//     console.log(x);
+//     return "hello world"
+//     var x = 10;
+// }
+// print();   // undefine
+// console.log(a);
+
+
+// function one(a,b){
+//     two(a,b)
+//     return a+b
+// }
+// function two(a,b){
+//     return a+b
+// }
+// console.log(one(25,24)+two(20,19));   //88
+
+
+
