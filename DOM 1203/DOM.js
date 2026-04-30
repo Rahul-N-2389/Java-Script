fd2a90bf77c55a401be7f1030cc33bd4a0e36d2a
Rahul-N-2389 <rahulneelam2389@gmail.com>
2026-04-30 17:06:45 +0530

    DOM-Creation of elements in dynamic way-12/03/26

---
+++ DOM 1203/DOM.js
@@ -0,0 +1,31 @@
+
+/**
+ * DOM (Document object Model)
+ * --------------------------
+ * Events
+ * API
+ * BOM
+ *
+ * => The parent of the HTML is "document" 
+ * => Parent of the document is "window"
+ * =>DOM is related to Browser. Because it is not open in terminal, it open only in browser
+ */
+
+// console.log(document);                    //get document
+// console.log(document.body);               //get body
+// console.log(document.head);               //get head
+// console.log(document.html);               //undefined beacause we should call it with another name
+// console.log(document.documentElement);    //here we get html
+// console.log(document.title);              //get title
+
+/**To get text in <P>tag in body
+ * -----------------------------
+ */
+// console.log(document.body.innerText);
+
+// //To change title name 
+// // --------------------
+// document.title ="DOM"
+// console.log(document.title);
+
+
